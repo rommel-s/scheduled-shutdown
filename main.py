fbb446e3eee358cad2309d1bf6e33309c8d05496
Rommel Santos <rommelrenne@gmail.com>
2022-03-08 22:42:01 -0300

    Started app GUI

--- main.py
+++ main.py
@@ -1,34 +1,60 @@
 import os
+import PySimpleGUI as sg
 from time import sleep as slp
 
-print('''
-  >>> DESLIGAMENTO PROGRAMADO <<<
-  
-Em quanto tempo o PC deve ser desligado?
+sg.theme('Reddit')
 
-Insira o valor em minutos:
+def inputTimeWindow():
+  layout = [
+    [sg.Text('>>> DESLIGAMENTO PROGRAMADO <<<')],
+    [sg.Text(' ')],
+    [sg.Text('Em quanto tempo o PC deve ser desligado?\nInsira o valor em minutos:')],
+    [sg.Input(key='timeToShutdown')],
+    [sg.Button('Desligar')]
+  ]
+  mainWindow = sg.Window('Desligamento Automático', layout=layout, finalize=True)
 
-''')
+  return mainWindow
+    
+def abortWindow():
+  layout = [
+    [sg.Text('Deseja Abortar?')],
+    [sg.Button('Sim'), sg.Button('Não')]
+  ]
 
-timeInput = int(input(">> "))
+  abortShutdownWindow = sg.Window('Abortar', layout=layout, finalize=True)
+  return abortShutdownWindow
 
-def continueQuestion():
-  abortcount = input('deseja abortar?: ')
-  if abortcount == 's' or abortcount == 'S':
-    return os.system("shutdown /a")
+def getTimeTinputinSeconds(timeInSeconds):
 
-
-def getTimeTinputinSeconds(timeToShutdown):
-
-  convertedTime = timeToShutdown * 60
+  convertedTime = int(timeInSeconds) * 60
 
   slp(3)
   print(f'O computador irá se desligar em {convertedTime} segundos')
+  # os.system(f'shutdown /s /t {convertedTime}')
   os.system(f'shutdown /s /t {convertedTime}')
-  
-  if convertedTime >= 60:
-    slp(5)
-    continueQuestion()
-    input("Você abortou a operação, pressione ENTER para fechar")
 
-getTimeTinputinSeconds(timeInput)
+firstWindow, secondWindow = inputTimeWindow(), None
+
+def abortQuestion():
+    return os.system("shutdown /a")
+
+while True:
+  window, event, values = sg.read_all_windows()
+  if window == firstWindow and event == sg.WIN_CLOSED:
+    break
+
+  if window == firstWindow and event == 'Desligar':
+    getTimeTinputinSeconds(values['timeToShutdown'])
+    secondWindow = abortWindow()
+    firstWindow.hide()
+
+  if window == secondWindow and event == sg.WIN_CLOSED:
+    break
+
+  if window == secondWindow and event == 'Sim':
+    abortQuestion()
+    secondWindow.close()
+
+  if window == secondWindow and event == 'Não':
+    secondWindow.close()
