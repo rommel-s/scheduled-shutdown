84909e111d0558411bbe6befb9b09219daca593a
Rommel Santos <rommelrenne@gmail.com>
2022-04-04 22:33:41 -0300

    v2.0.3 - User now can choose between minutes or hours

--- DesligamentoProgramado.py
+++ DesligamentoProgramado.py
@@ -6,10 +6,13 @@ class ScheduledShutdown():
     sg.theme('Reddit')
 
     layout = [
-      [sg.Text('>>> DESLIGAMENTO PROGRAMADO <<<')],
+      [sg.Text('    DESLIGAMENTO PROGRAMADO    ')],
       [sg.Text(' ')],
-      [sg.Text('Em quanto tempo o PC deve ser desligado?\nInsira o valor em minutos:')],
+      [sg.Text('Em quanto tempo o PC deve ser desligado?')],
       [sg.Input(key='timeToShutdown', size=(40,5))],
+      [sg.Text(' ')],
+      [sg.Text('Que medida de tempo devo usar?')],
+      [sg.Radio('Minutos     ', "RADIO1", key='timeInMinutes', default=True, size=(10,1)), sg.Radio('Horas', "RADIO1", key='timeInHours', size=(10,1))],
       [sg.Button('Desligar')],
       [sg.HorizontalSeparator()],
       [sg.Multiline(key='Log'+sg.WRITE_ONLY_KEY, size=(40,5), disabled = True, no_scrollbar = True)],
@@ -27,22 +30,44 @@ class ScheduledShutdown():
 
       if self.event == 'Desligar':
         shut = int(self.values['timeToShutdown'])
-        self.getTimeInputinSeconds(shut)
-        self.mainWindow['Log'+sg.WRITE_ONLY_KEY].print(f'O computador irá se desligar em {shut*60} segundos')
+        self.checkAmountOfTime(shut)
 
       if self.event == 'Abortar Desligamento':
-        self.mainWindow['Log'+sg.WRITE_ONLY_KEY].print(f'Desligamento cancelado!', text_color='red')
         self.abortShutdown()
 
+  def checkAmountOfTime(self, timeToCheck):
+
+    if self.values['timeInMinutes'] == True:
+      self.getTimeInputinSeconds(timeToCheck)
+      self.showLog(timeToCheck)
+    
+    if self.values['timeInHours'] == True:
+      self.setTimeInHours(timeToCheck)
+      self.showLog(timeToCheck)
+
+  def setTimeInHours(self, getTimeToConvert):
+    hourToMinute = getTimeToConvert * 60
+    minuteInSeconds = hourToMinute * 60
+    print(f'shutdown /s /t {minuteInSeconds}')
+    return os.system(f'shutdown /s /t {minuteInSeconds}')
     
   def getTimeInputinSeconds(self, timeInMinutes):
-
     timeInSeconds = timeInMinutes * 60
-    os.system(f'shutdown /s /t {timeInSeconds}')
-    #print(f'shutdown /s /t {timeInSeconds}')
+    print(f'shutdown /s /t {timeInSeconds}')
+    return os.system(f'shutdown /s /t {timeInSeconds}')
+    
 
   def abortShutdown(self):
+    self.mainWindow['Log'+sg.WRITE_ONLY_KEY].print(f'Desligamento cancelado!', text_color='red')
     return os.system("shutdown /a")
+
+  def showLog(self, inputtedTime):
+
+    if self.values['timeInMinutes'] == True:
+      self.mainWindow['Log'+sg.WRITE_ONLY_KEY].print(f'O computador irá se desligar em {inputtedTime} minutos')
+    
+    if self.values['timeInHours'] == True:
+      return self.mainWindow['Log'+sg.WRITE_ONLY_KEY].print(f'O computador irá se desligar em {inputtedTime} horas')
 
 shutdown = ScheduledShutdown()
 shutdown.startApp()
